1cd24ac22eec8bf96655fb19be5cd2d8bd1585bc
Jimena Miguez <jimeblau@gmail.com>
2022-08-25 15:53:23 +0200

    delete comments

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -13,38 +13,14 @@
         <option value="developer">Web Developer</option>
         <option value="designer">Web Designer</option>
       </select>
-      <!-- EXPLANATION: there are 2 ways of tracking values of checked checkboxes -->
-      <!-- WAY ONE - SINGLE CHECKBOX-->
-      <!-- 1) Add the checkboxes -->
       <div class="terms">
-        <!-- 2) Track the value of the checked checkbox setting the value
-        of v-model to "terms". The property terms is a boolean. -->
         <input type="checkbox" v-model="terms" required />
         <label>Accept terms and conditions</label>
-      </div>
-      <!-- WAY TWO - MULTIPLE CHECKBOXES -->
-      <div>
-        <!-- A) Create the checkboxes. Add a value to each of them.
-        Set the v-model of all of them to names. 
-        The names property is an array -->
-        <input type="checkbox" value="Fede" v-model="names" />
-        <label>Fede</label>
-        <input type="checkbox" value="Ariel" v-model="names" />
-        <label>Ariel</label>
-        <input type="checkbox" value="Jimena" v-model="names" />
-        <label>Jimena</label>
       </div>
     </form>
     <p>Email: {{ email }}</p>
     <p>Password: {{ password }}</p>
     <p>Role: {{ role }}</p>
-    <!-- 4) Ouput the value of terms just to see it´s a boolean -->
-    <p>Terms accepted: {{ terms }}</p>
-    <!-- C) Output the checked value. Use v-for ´cause it´s an array -->
-    <ul>
-      Selected names
-      <li v-for="name in names">{{ name }}</li>
-    </ul>
   </div>
 </template>
 <script>
@@ -54,13 +30,7 @@ export default {
       email: "",
       password: "",
       role: "designer",
-      /* 3) Create the property terms to store the value of the 
-      checked checkbox and set it to false. If I want the checkbox
-      to be initially checked by dafault, I set term to true*/
       terms: false,
-      /* B) Create the names property to store the value of
-      the checked checkboxes and set it to an empty array. */
-      names: [],
     };
   },
 };
